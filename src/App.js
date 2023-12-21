e480cd20321d1f04a98db23973b5a50edfaec544
Shauna845 <shauna10@hotmail.co.uk>
2023-12-21 14:57:44 +0000

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ export default function App() {
         <a
           href="https://github.com/Shauna845/react-weather-app"
           target="_blank"
+          rel="noreferrer"
         >
           {" "}
           Open-source code{" "}
@@ -19,6 +20,7 @@ export default function App() {
         <a
           href="https://www.shecodes.io/graduates/65141-shauna-bratten"
           target="_blank"
+          rel="noreferrer"
         >
           {" "}
           Shauna Bratten{" "}
