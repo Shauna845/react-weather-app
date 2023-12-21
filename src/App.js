41cd46e6fe6c4f35ff91c610299e9c91cf680c88
Shauna845 <shauna10@hotmail.co.uk>
2023-12-21 15:20:25 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,28 +4,30 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <h1>React Weather App</h1>
-      <Weather />
-      <br />
-      <footer>
-        <a
-          href="https://github.com/Shauna845/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Open-source code{" "}
-        </a>{" "}
-        created by{" "}
-        <a
-          href="https://www.shecodes.io/graduates/65141-shauna-bratten"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Shauna Bratten{" "}
-        </a>
-      </footer>
+      <div classname="container">
+        <h1>React Weather App</h1>
+        <Weather />
+        <br />
+        <footer>
+          <a
+            href="https://github.com/Shauna845/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Open-source code{" "}
+          </a>{" "}
+          created by{" "}
+          <a
+            href="https://www.shecodes.io/graduates/65141-shauna-bratten"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Shauna Bratten{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
