d95a290faabf6e0ceb1c5a9804a0f1d905ec862e
Shauna845 <shauna10@hotmail.co.uk>
2024-01-03 21:45:30 +0000

    Updated weather

--- src/Weather.js
+++ src/Weather.js
@@ -22,7 +22,7 @@ export default function Weather(props) {
 
   function search() {
     const apiKey = "6b137f70277395c99f9b23f929069581";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
 
