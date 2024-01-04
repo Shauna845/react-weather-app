334ff94f7e096e59b8e63b26031ae94201c2e09c
Shauna845 <shauna10@hotmail.co.uk>
2024-01-04 18:00:27 +0000

    Connected api call to get daily forecast

--- src/Weather.js
+++ src/Weather.js
@@ -23,7 +23,7 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "97f8e93f00107773f88eafd933ce86b7";
+    const apiKey = "5201594abea9f3e38b70e65b11a80c24";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
